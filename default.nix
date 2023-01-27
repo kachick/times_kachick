5aa7be0d901a36a28a6212e90bae5bf1e5ec0289
Kenichi Kamiya <kachick1@gmail.com>
2023-01-27 13:59:13 +0900

    Add editor config

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8eabb500ebf5dcb9a40024c1150ef348cefa8320.tar.gz") { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nil
+    pkgs.nixpkgs-fmt
+    pkgs.dprint
+  ];
+}
